41838a8443c1658f794c8154fcdc9c3039011503
Francisco Saez Munoz <f.saez@duocuc.cl>
2023-11-13 08:13:56 -0300

    modified:   InventaPRO/__pycache__/settings.cpython-39.pyc 	modified:   InventaPRO/settings.py 	modified:   Web_app/__pycache__/views.cpython-39.pyc 	modified:   Web_app/static/core/css/style1.css 	new file:   Web_app/static/core/img/1571858238238.jpg 	new file:   Web_app/static/core/img/UHLOPCFDJBFT3EN3N5RQQD4FHA.jpg 	new file:   Web_app/static/core/img/adobestock-294179798-edited-scaled.jpeg 	new file:   Web_app/static/core/img/descarga.jpg 	new file:   Web_app/static/core/img/kidfriendly-ideas-for-an-ideal-vacation-in-ventura-california-449681a25a5b437193917b174acf7bd6.png 	new file:   Web_app/static/core/img/spring-strawberries.jpeg 	modified:   Web_app/templates/core/vista_enc_bodega.html 	modified:   Web_app/views.py

--- InventaPRO/settings.py
+++ InventaPRO/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 ROOT_URLCONF = 'InventaPRO.urls'
 
 TEMPLATES = [
--- Web_app/views.py
+++ Web_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
 from .models import Producto
 
 # Create your views here.
@@ -8,11 +9,11 @@ def index(request):
     return render(request, 'core/index.html')
 
 def vista_enc_bodega(request):
-    categoria = request.GET.get('categoria')
-    estado = request.GET.get('estado')
+    data = Producto.objects.all()  # Obtén todos los productos
 
     # Filtrar productos en función de la categoría y el estado
-    data = Producto.objects.all()  # Obtén todos los productos
+    categoria = request.GET.get('categoria')
+    estado = request.GET.get('estado')
 
     if categoria:
         data = data.filter(categoria=categoria)
@@ -20,7 +21,28 @@ def vista_enc_bodega(request):
     if estado:
         data = data.filter(estado=estado)
 
-    context = {"productos": data}
+    selec_categorias = Producto.objects.values_list('categoria', flat=True).distinct()
+    context = {"productos": data, "categorias": selec_categorias}
+
+    if request.method == 'POST':
+        print("Entré al bloque POST")  # Mensaje de depuración
+
+        nombre_producto = request.POST['productName']
+        categoria = request.POST['productCategory']
+        cantidad = request.POST.get('productAmount', 0)
+        unidad = request.POST['productUnitType']
+
+        print(f"Datos del formulario: {nombre_producto}, {categoria}, {cantidad}, {unidad}")  # Mensaje de depuración
+
+        producto = Producto(nombre_producto=nombre_producto, categoria=categoria, cantidad=cantidad, unidad=unidad)
+        producto.save()
+
+        messages.success(request, '¡Producto registrado con éxito!')
+
+        print("Producto guardado en la base de datos")  # Mensaje de depuración
+
+        return redirect('core:vista_enc_bodega')
+
     return render(request, 'core/vista_enc_bodega.html', context)
 
 
