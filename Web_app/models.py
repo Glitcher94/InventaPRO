c358f6fc8456404ab6c9e18d7d4ed42b22573041
Francisco Saez Munoz <f.saez@duocuc.cl>
2023-11-20 16:27:14 -0300

    modified:   InventaPRO/__pycache__/settings.cpython-39.pyc 	modified:   InventaPRO/__pycache__/urls.cpython-39.pyc 	modified:   InventaPRO/urls.py 	modified:   Web_app/__pycache__/models.cpython-39.pyc 	modified:   Web_app/__pycache__/views.cpython-39.pyc 	new file:   Web_app/migrations/0004_alter_detallesolicitud_options_and_more.py 	new file:   Web_app/migrations/0005_remove_movimientosinventario_id_usuario_and_more.py 	new file:   Web_app/migrations/0006_movimientosinventario_id_usuario.py 	new file:   Web_app/migrations/0007_usuario_groups_usuario_is_superuser_and_more.py 	new file:   Web_app/migrations/0008_remove_usuario_groups_remove_usuario_is_superuser_and_more.py 	new file:   Web_app/migrations/0009_alter_usuario_clave_alter_usuario_correo.py 	new file:   Web_app/migrations/__pycache__/0004_alter_detallesolicitud_options_and_more.cpython-39.pyc 	new file:   Web_app/migrations/__pycache__/0005_remove_movimientosinventario_id_usuario_and_more.cpython-39.pyc 	new file:   Web_app/migrations/__pycache__/0006_movimientosinventario_id_usuario.cpython-39.pyc 	new file:   Web_app/migrations/__pycache__/0007_usuario_groups_usuario_is_superuser_and_more.cpython-39.pyc 	new file:   Web_app/migrations/__pycache__/0008_remove_usuario_groups_remove_usuario_is_superuser_and_more.cpython-39.pyc 	new file:   Web_app/migrations/__pycache__/0009_alter_usuario_clave_alter_usuario_correo.cpython-39.pyc 	modified:   Web_app/models.py 	modified:   Web_app/static/core/css/estilo.css 	modified:   Web_app/static/core/css/style1.css 	new file:   Web_app/templates/core/acceso_admin.html 	modified:   Web_app/templates/core/index.html 	modified:   Web_app/templates/core/vista_enc_bodega.html 	modified:   Web_app/templates/core/vista_jefe_dideco.html 	new file:   Web_app/templates/core/vista_trab_social.html 	modified:   Web_app/views.py

--- Web_app/models.py
+++ Web_app/models.py
@@ -6,6 +6,13 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import Permission
+from django.utils.translation import gettext_lazy as _
+
+
 
 
 class AuthGroup(models.Model):
@@ -77,17 +84,6 @@ class AuthUserUserPermissions(models.Model):
         unique_together = (('user', 'permission'),)
 
 
-class DetalleSolicitud(models.Model):
-    id_detalle_sol = models.FloatField(primary_key=True)
-    id_solicitud = models.ForeignKey('Solicitud', models.DO_NOTHING, db_column='id_solicitud', blank=True, null=True)
-    id_producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='id_producto', blank=True, null=True)
-    cantidad_solicitada = models.FloatField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'detalle_solicitud'
-
-
 class DjangoAdminLog(models.Model):
     action_time = models.DateTimeField()
     object_id = models.TextField(blank=True, null=True)
@@ -136,10 +132,11 @@ class DjangoSession(models.Model):
 class MovimientosInventario(models.Model):
     id_movimiento = models.FloatField(primary_key=True)
     id_producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='id_producto', blank=True, null=True)
-    tipo_movimiento = models.CharField(max_length=50, blank=True, null=True)
     cantidad = models.FloatField(blank=True, null=True)
+    tipo_movimiento = models.CharField(max_length=50, blank=True, null=True)
+    id_usuario = models.ForeignKey('Usuario', models.DO_NOTHING, db_column='id_usuario', blank=True, null=True)
+    id_solicitud = models.ForeignKey('Solicitud', models.DO_NOTHING, db_column='id_solicitud', blank=True, null=True)
     fecha_movimiento = models.DateField(blank=True, null=True)
-    id_usuario = models.FloatField(blank=True, null=True)
 
     class Meta:
         db_table = 'movimientos_inventario'
@@ -161,21 +158,43 @@ class Producto(models.Model):
 
 class Solicitud(models.Model):
     id_solicitud = models.BigIntegerField(primary_key=True)
-    id_usuario = models.BigIntegerField(blank=True, null=True)
     fecha_solicitud = models.DateField(blank=True, null=True)
+    tipo_solicitud = models.CharField(max_length=20, blank=True, null=True)
     estado_solicitud = models.CharField(max_length=20, blank=True, null=True)
-    ruta_archivo = models.CharField(max_length=20, blank=True, null=True)
+    beneficiario = models.CharField(max_length=30, blank=True, null=True)
 
     class Meta:
         db_table = 'solicitud'
 
 
+def pin_validator(value):
+    if not value.isdigit() or len(value) != 5:
+        raise ValidationError('El PIN debe contener exactamente 5 dígitos.')
+
+
 class Usuario(models.Model):
     id_usuario = models.FloatField(primary_key=True)
     nombre_usuario = models.CharField(max_length=255, blank=True, null=True)
-    correo = models.CharField(max_length=30, blank=True, null=True)
-    clave = models.FloatField(blank=True, null=True)
+    correo = models.EmailField(unique=True, max_length=30, blank=True, null=True)
+    clave = models.CharField(max_length=255, null=True)
     rol = models.CharField(max_length=50, blank=True, null=True)
+
+    objects = BaseUserManager()
+
+    def __str__(self):
+        return self.nombre_usuario  # O el campo que prefieras mostrar como representación de cadena
 
     class Meta:
         db_table = 'usuario'
+
+
+
+
+class DetalleSolicitud(models.Model):
+    id_detalle_sol = models.FloatField(primary_key=True)
+    id_solicitud = models.ForeignKey('Solicitud', models.DO_NOTHING, db_column='id_solicitud', blank=True, null=True)
+    id_producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='id_producto', blank=True, null=True)
+    cantidad_solicitada = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'detalle_solicitud'
--- Web_app/views.py
+++ Web_app/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.contrib import messages
 from .models import Producto, Usuario
+
+
 
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Realiza la validación del usuario
+        try:
+            user = Usuario.objects.get(correo=email, clave=password)
+            if user.rol == 'Administrador':
+                return HttpResponseRedirect('core/acceso_admin')
+            elif user.rol == 'Trabajadora Social':
+                return HttpResponseRedirect('core/vista_trab_social') 
+            elif user.rol == 'DIDECO':
+                return HttpResponseRedirect('core/vista_jefe_dideco')
+            elif user.rol == 'Bodeguero':
+                return HttpResponseRedirect('core/vista_enc_bodega')
+            elif user.rol == 'Encargada inventario social':
+                return HttpResponseRedirect('core/vista_enc_bodega')
+
+        except Usuario.DoesNotExist:
+            # Manejar el caso en que el usuario no exista o las credenciales sean incorrectas
+            return render(request, 'index.html', {'error': 'Credenciales incorrectas'})
+
     return render(request, 'core/index.html')
+
 
 def vista_enc_bodega(request):
     data = Producto.objects.all()  # Obtén todos los productos
@@ -22,7 +47,7 @@ def vista_enc_bodega(request):
         data = data.filter(estado=estado)
 
     selec_categorias = Producto.objects.values_list('categoria', flat=True).distinct()
-    context = {"productos": data, "categorias": selec_categorias}
+    context = {"inventario": data, "categorias": selec_categorias}
 
     if request.method == 'POST':
         print("Entré al bloque POST")  # Mensaje de depuración
@@ -38,6 +63,14 @@ def vista_enc_bodega(request):
 
         producto = Producto(nombre_producto=nombre_producto, categoria=categoria, cantidad=cantidad, unidad=unidad,
                              orden_compra=orden_compra)
+        
+        if cantidad > 0:
+            producto.estado = 'En stock'
+        elif cantidad is None:
+            producto.estado = 'Desconocido'
+        else:
+            producto.estado = 'Agotado'
+
         producto.save()
 
         messages.success(request, '¡Producto registrado con éxito!')
@@ -50,27 +83,67 @@ def vista_enc_bodega(request):
 
 
 def vista_jefe_dideco(request):
-    data = Producto.objects.all()
-    context = {"productos": data}
+    data = Producto.objects.all()  # Obtén todos los productos
+
+    # Filtrar productos en función de la categoría y el estado
+    categoria = request.GET.get('categoria')
+    estado = request.GET.get('estado')
+
+    if categoria:
+        data = data.filter(categoria=categoria)
+
+    if estado:
+        data = data.filter(estado=estado)
+
+    selec_categorias = Producto.objects.values_list('categoria', flat=True).distinct()
+    context = {"inventario": data, "categorias": selec_categorias}
 
     if request.method == 'POST':
         print("Entré al bloque POST")  # Mensaje de depuración
 
         nombre_usuario = request.POST['userName']
-        rol = request.POST['userRol']
         correo = request.POST['userEmail']
         clave = request.POST['userPin']
+        rol = request.POST['userRol']
 
-        print(f"Datos del formulario: {nombre_usuario}, {rol}, {correo}, {clave}")  # Mensaje de depuración
+        # Mensaje de depuración
+        print(f"Datos del formulario: {nombre_usuario}, {correo}, {clave}, {rol}")
 
-        usuario = Usuario(nombre_usuario=nombre_usuario, rol=rol, correo=correo,clave=clave)
+        usuario = Usuario(nombre_usuario=nombre_usuario, correo=correo, clave=clave, rol=rol)
+
         usuario.save()
 
-        messages.success(request, '¡Usuario registrado con éxito!')
+        messages.success(request, '¡Producto registrado con éxito!')
 
-        print("Usuario guardado en la base de datos")  # Mensaje de depuración
+        print("Producto guardado en la base de datos")  # Mensaje de depuración
 
         return redirect('core/vista_jefe_dideco')
 
-
     return render(request, 'core/vista_jefe_dideco.html', context)
+
+
+
+
+def vista_trab_social(request):
+    data = Producto.objects.all()  # Obtén todos los productos
+
+    # Filtrar productos en función de la categoría y el estado
+    categoria = request.GET.get('categoria')
+    estado = request.GET.get('estado')
+
+    if categoria:
+        data = data.filter(categoria=categoria)
+
+    if estado:
+        data = data.filter(estado=estado)
+
+    selec_categorias = Producto.objects.values_list('categoria', flat=True).distinct()
+    selec_productos = Producto.objects.values_list('nombre_producto', flat=True).distinct()
+    context = {"inventario": data, "categorias": selec_categorias, "productos": selec_productos }
+
+    return render(request, 'core/vista_trab_social.html', context)
+
+
+
+def acceso_admin(request):
+    return render(request, 'core/acceso_admin.html')
